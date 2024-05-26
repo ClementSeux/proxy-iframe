f88ab3296dae14e5f8e7e923b2e2d4f6ec24a34e
Clément SEUX <clement.seux@axxes.fr>
2024-05-26 12:46:42 +0200

    feat: Update /shop route to use HTTPS and remove X-Frame-Options header

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const axios = require("axios"); // Pour effectuer des requêtes HTTP
-const express = require("express");
-const axios = require("axios"); // Pour effectuer des requêtes HTTP
 const https = require("https");
 const fs = require("fs");
 
@@ -35,12 +33,15 @@ app.get("/shop", async (req, res) => {
         app.get("/shop", async (req, res) => {
             try {
                 // Effectuer une requête GET vers www.shopmium.com
-                const response = await axios.get("https://www.shopmium.com/fr/", {
-                    headers: {
-                        // Supprimer l'en-tête X-Frame-Options
-                        "X-Frame-Options": undefined,
-                    },
-                });
+                const response = await axios.get(
+                    "https://www.shopmium.com/fr/",
+                    {
+                        headers: {
+                            // Supprimer l'en-tête X-Frame-Options
+                            "X-Frame-Options": undefined,
+                        },
+                    }
+                );
 
                 response.data = response.data.replace(
                     'href="https://www.shopmium.com/fr/',
@@ -50,8 +51,13 @@ app.get("/shop", async (req, res) => {
                 // Renvoyer le contenu HTML, CSS et JavaScript
                 res.send(response.data);
             } catch (error) {
-                console.error("Erreur lors de la requête vers Shopmium :", error.message);
-                res.status(500).send("Erreur lors de la récupération du contenu.");
+                console.error(
+                    "Erreur lors de la requête vers Shopmium :",
+                    error.message
+                );
+                res.status(500).send(
+                    "Erreur lors de la récupération du contenu."
+                );
             }
         });
 
