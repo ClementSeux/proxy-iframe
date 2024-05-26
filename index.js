dbbe7e4bfde36933d3ca82409197bf5eecf19480
Clement Seux <clement.seux@gmail.com>
2024-05-26 12:22:07 +0200

    feat: proxy pour iframe

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios"); // Pour effectuer des requêtes HTTP
+
+const app = express();
+const PORT = process.env.PORT || 3004;
+
+// Middleware pour gérer les requêtes vers /shop
+app.get("/shop", async (req, res) => {
+    try {
+        // Effectuer une requête GET vers www.shopmium.com
+        const response = await axios.get("https://www.shopmium.com/fr/", {
+            headers: {
+                // Supprimer l'en-tête X-Frame-Options
+                "X-Frame-Options": undefined,
+            },
+        });
+        // replace all href= with localhost:3004/shop
+        response.data = response.data.replace(
+            'href="https://www.shopmium.com/fr/',
+            'href="http:/192.168.1.158:3004/shop/'
+        );
+
+        // Renvoyer le contenu HTML, CSS et JavaScript
+        res.send(response.data);
+    } catch (error) {
+        console.error(
+            "Erreur lors de la requête vers Shopmium :",
+            error.message
+        );
+        res.status(500).send("Erreur lors de la récupération du contenu.");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
+});
