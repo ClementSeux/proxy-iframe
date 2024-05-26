8509324c444f3b067844dbb4027f47dbc0bae67c
Clément SEUX <clement.seux@axxes.fr>
2024-05-26 12:52:51 +0200

    feat: Update /shop route to use HTTPS, remove X-Frame-Options header, and add SSL certificate

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3004;
 
+const httpsOptions = {
+    key: fs.readFileSync(
+        "/etc/letsencrypt/live/save.back.clementseux.me/privkey.pem"
+    ),
+    cert: fs.readFileSync(
+        "/etc/letsencrypt/live/save.back.clementseux.me/fullchain.pem"
+    ),
+};
+
 // Middleware pour gérer les requêtes vers /shop
 app.get("/shop", async (req, res) => {
     try {
@@ -16,55 +25,6 @@ app.get("/shop", async (req, res) => {
                 "X-Frame-Options": undefined,
             },
         });
-        // replace all href= with localhost:3004/shop
-        const app = express();
-        const PORT = process.env.PORT || 3004;
-
-        const httpsOptions = {
-            key: fs.readFileSync(
-                "/etc/letsencrypt/live/save.back.clementseux.me/privkey.pem"
-            ),
-            cert: fs.readFileSync(
-                "/etc/letsencrypt/live/save.back.clementseux.me/fullchain.pem"
-            ),
-        };
-
-        // Middleware pour gérer les requêtes vers /shop
-        app.get("/shop", async (req, res) => {
-            try {
-                // Effectuer une requête GET vers www.shopmium.com
-                const response = await axios.get(
-                    "https://www.shopmium.com/fr/",
-                    {
-                        headers: {
-                            // Supprimer l'en-tête X-Frame-Options
-                            "X-Frame-Options": undefined,
-                        },
-                    }
-                );
-
-                response.data = response.data.replace(
-                    'href="https://www.shopmium.com/fr/',
-                    'href="http:/172.232.32.176:3004/shop/'
-                );
-
-                // Renvoyer le contenu HTML, CSS et JavaScript
-                res.send(response.data);
-            } catch (error) {
-                console.error(
-                    "Erreur lors de la requête vers Shopmium :",
-                    error.message
-                );
-                res.status(500).send(
-                    "Erreur lors de la récupération du contenu."
-                );
-            }
-        });
-
-        https.createServer(httpsOptions, app).listen(PORT, () => {
-            console.log(`Serveur en écoute sur le port ${PORT}`);
-        });
-
         // Renvoyer le contenu HTML, CSS et JavaScript
         res.send(response.data);
     } catch (error) {
@@ -76,6 +36,6 @@ app.get("/shop", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+https.createServer(httpsOptions, app).listen(PORT, () => {
     console.log(`Serveur en écoute sur le port ${PORT}`);
 });
