84fd0953c991f518da07d9213cbd243f1d34e521
Clément SEUX <clement.seux@axxes.fr>
2024-06-06 22:53:05 +0200

    Update index.js to replace links and script sources with modified URLs

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get("/", async (req, res) => {
         // Exemple: /shop?link=https://www.shopmium.com
         const link = req.query.link;
         console.log("link", link);
+        const baseSite = link.split("/")[2];
+        console.log("baseSite", baseSite);
         // Effectuer une requête GET vers www.shopmium.com
         const response = await axios.get(link, {
             headers: {
@@ -31,12 +33,7 @@ app.get("/", async (req, res) => {
                 "X-Frame-Options": undefined,
             },
         });
-        console.log("response received");
-        console.log("type:", typeof response.data);
         let oldHTML = response.data;
-        let str = "test de string";
-        str = str.replace("test", "test2");
-        console.log("str:", str);
 
         // remple les liens par "https://www.save.back.me:3004/?link=lien"
         oldHTML = oldHTML.replace(/href="([^"]*)"/g, function (match, p1) {
@@ -45,7 +42,7 @@ app.get("/", async (req, res) => {
             }
             return (
                 'href="https://www.save.back.clementseux.me:3004/?link=' +
-                link +
+                baseSite +
                 p1 +
                 '"'
             );
@@ -58,24 +55,12 @@ app.get("/", async (req, res) => {
             }
             return (
                 'src="https://www.save.back.clementseux.me:3004/?link=' +
-                link +
+                baseSite +
                 p1 +
                 '"'
             );
         });
 
-        // oldHTML = oldHTML.replace(
-        //     "</body>",
-        //     '<div id="logDiv" style="position: fixed; top: 0; right: 0; background-color: white; z-index: 10000; padding: 10px; border: 1px solid black;"></div></body>'
-        // );
-
-        // oldHTML = oldHTML.replace(
-        //     "</body>",
-        //     '<script type="text/javascript">window.onload = function() { function handleURLChange(event) {event.preventDefault();console.log("URL has changed!");}window.addEventListener("hashchange", handleURLChange); window.addEventListener("popstate", handleURLChange);};window.console.log = function(message) {var logDiv = document.getElementById("logDiv");logDiv.innerHTML += message + "<br>";</script></body>'
-        // );
-        // // Renvoyer le contenu HTML, CSS et JavaScript
-        console.log("response modifiée");
-        console.log(oldHTML);
         res.send(oldHTML);
     } catch (error) {
         console.error(
