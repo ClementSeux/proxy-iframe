5a59182b260190d15df6380561fa7f5c1f123084
Clément SEUX <clement.seux@axxes.fr>
2024-06-07 00:56:49 +0200

    Update index.js to log the query parameter in handleRequest

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function handleRequest(req, res, verb) {
         console.log("requete reçue");
         // recupere l'attribut link dans l'url
         // Exemple: /shop?link=https://www.shopmium.com
+        console.log(req.query);
         const link = req.query.link;
         console.log("link", link);
         // Extrait le nom de domaine de l'URL ex: https://www.google.com/search => https://www.google.com
